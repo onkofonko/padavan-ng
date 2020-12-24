ff611d146c52f4899e50bc9f03a3956dccaecbf2
Alexey <4653240-dm38@users.noreply.gitlab.com>
2020-12-24 12:05:54 +0300

    mtk-wifi: fix update basic rate bitmap

--- trunk/proprietary/rt_wifi/rtpci/4.1.X.X/mt76x3/common/mlme.c
+++ trunk/proprietary/rt_wifi/rtpci/4.1.X.X/mt76x3/common/mlme.c
@@ -1979,6 +1979,8 @@ VOID UpdateBasicRateBitmap(RTMP_ADAPTER *pAdapter)
         return;
     }
 
+	bitmap = pAdapter->CommonCfg.BasicRateBitmap;  /* renew bitmap value */
+
     for(i=0; i<MAX_LEN_OF_SUPPORTED_RATES; i++)
     {
         sup_p[i] &= 0x7f;
