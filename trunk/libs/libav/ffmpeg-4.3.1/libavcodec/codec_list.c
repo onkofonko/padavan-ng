b768a76e50a53450c417dfd62825f283fc36e2c2
Mahtab Uddin <mahtabctg@gmail.com>
2021-07-03 18:59:53 +0600

    Minor fixes and cleanup

---
+++ trunk/libs/libav/ffmpeg-4.3.1/libavcodec/codec_list.c
@@ -0,0 +1,4 @@
+static const AVCodec * const codec_list[] = {
+    &ff_alac_decoder,
+    &ff_flac_decoder,
+    NULL };
---
+++ trunk/libs/libav/ffmpeg-4.3.1/libavcodec/parser_list.c
@@ -0,0 +1,7 @@
+static const AVCodecParser * const parser_list[] = {
+    &ff_aac_parser,
+    &ff_ac3_parser,
+    &ff_dca_parser,
+    &ff_flac_parser,
+    &ff_mpegaudio_parser,
+    NULL };
