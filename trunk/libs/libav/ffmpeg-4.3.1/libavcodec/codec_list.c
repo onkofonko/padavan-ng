f11216dcbbf1e034954f35527fd86226f1297931
Mahtab Uddin <mahtabctg@gmail.com>
2021-11-04 01:43:58 +0600

    libconfuse to v3.3

--- trunk/libs/libav/ffmpeg-4.3.1/libavcodec/codec_list.c
+++
@@ -1,4 +0,0 @@
-static const AVCodec * const codec_list[] = {
-    &ff_alac_decoder,
-    &ff_flac_decoder,
-    NULL };
--- trunk/libs/libav/ffmpeg-4.3.1/libavcodec/parser_list.c
+++
@@ -1,7 +0,0 @@
-static const AVCodecParser * const parser_list[] = {
-    &ff_aac_parser,
-    &ff_ac3_parser,
-    &ff_dca_parser,
-    &ff_flac_parser,
-    &ff_mpegaudio_parser,
-    NULL };
