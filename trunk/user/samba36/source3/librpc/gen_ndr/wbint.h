5032b0c7469b67fbd40d047dfd7e7f9fcfdcd976
Mahtab <mahtabctg@gmail.com>
2022-02-10 04:40:49 +0600

    minor build fix

--- trunk/user/samba36/source3/librpc/gen_ndr/wbint.h
+++ trunk/user/samba36/source3/librpc/gen_ndr/wbint.h
@@ -28,8 +28,8 @@ struct wbint_TransIDArray {
 };
 
 struct wbint_userinfo {
-	const char *acct_name;/* [unique,charset(UTF8)] */
-	const char *full_name;/* [charset(UTF8),unique] */
+	const char *acct_name;/* [charset(UTF8),unique] */
+	const char *full_name;/* [unique,charset(UTF8)] */
 	const char *homedir;/* [charset(UTF8),unique] */
 	const char *shell;/* [unique,charset(UTF8)] */
 	uint64_t primary_gid;
@@ -83,8 +83,8 @@ struct wbint_LookupSid {
 
 	struct {
 		enum lsa_SidType *type;/* [ref] */
-		const char **domain;/* [ref,charset(UTF8)] */
-		const char **name;/* [ref,charset(UTF8)] */
+		const char **domain;/* [charset(UTF8),ref] */
+		const char **name;/* [charset(UTF8),ref] */
 		NTSTATUS result;
 	} out;
 
@@ -107,7 +107,7 @@ struct wbint_LookupSids {
 
 struct wbint_LookupName {
 	struct {
-		const char *domain;/* [ref,charset(UTF8)] */
+		const char *domain;/* [charset(UTF8),ref] */
 		const char *name;/* [ref,charset(UTF8)] */
 		uint32_t flags;
 	} in;
@@ -137,7 +137,7 @@ struct wbint_Sid2Uid {
 
 struct wbint_Sid2Gid {
 	struct {
-		const char *dom_name;/* [unique,charset(UTF8)] */
+		const char *dom_name;/* [charset(UTF8),unique] */
 		struct dom_sid *sid;/* [ref] */
 	} in;
 
@@ -179,7 +179,7 @@ struct wbint_Uid2Sid {
 
 struct wbint_Gid2Sid {
 	struct {
-		const char *dom_name;/* [charset(UTF8),unique] */
+		const char *dom_name;/* [unique,charset(UTF8)] */
 		uint64_t gid;
 	} in;
 
@@ -312,7 +312,7 @@ struct wbint_LookupRids {
 	} in;
 
 	struct {
-		const char **domain_name;/* [ref,charset(UTF8)] */
+		const char **domain_name;/* [charset(UTF8),ref] */
 		struct wbint_Principals *names;/* [ref] */
 		NTSTATUS result;
 	} out;
