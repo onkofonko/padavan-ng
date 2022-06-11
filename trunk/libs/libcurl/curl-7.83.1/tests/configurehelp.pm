8d354657f209c766c2d377b0b0564b25be084b11
Mahtab Uddin <mahtabctg@gmail.com>
2022-06-11 08:09:47 +0000

    Updated to Curl-7.83.1

---
+++ trunk/libs/libcurl/curl-7.83.1/tests/configurehelp.pm
@@ -0,0 +1,23 @@
+# This is a generated file.  Do not edit.
+
+package configurehelp;
+
+use strict;
+use warnings;
+use Exporter;
+
+use vars qw(
+    @ISA
+    @EXPORT_OK
+    $Cpreprocessor
+    );
+
+@ISA = qw(Exporter);
+
+@EXPORT_OK = qw(
+    $Cpreprocessor
+    );
+
+$Cpreprocessor = '/opt/padavan-ng/trunk/../toolchain/out/bin/mipsel-linux-uclibc-gcc -mips32r2 -march=mips32r2 -mtune=1004kc -E -isystem /opt/padavan-ng/trunk/stage/include -isystem //include';
+
+1;
