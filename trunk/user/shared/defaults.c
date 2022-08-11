6c492db2dc4201cedef2a21ee5aba8d88701b25d
Mahtab Uddin <mahtabctg@gmail.com>
2022-08-11 17:48:03 +0000

    Default 2.4G enabled fix removed ax mode

--- trunk/user/shared/defaults.c
+++ trunk/user/shared/defaults.c
@@ -269,7 +269,6 @@ struct nvram_pair router_defaults[] = {
 	/* 2G Wireless parameters */
 	{ "rt_country_code", DEF_WLAN_2G_CC },
 	{ "rt_ssid", DEF_WLAN_2G_SSID },
-	{ "rt_gmode", "6" },			/* b/g/n/ax mixed */
 	{ "rt_gmode", "5" },			/* g/n mixed */
 	{ "rt_mcs_mode", "0" },
 	{ "rt_channel", "0" },
