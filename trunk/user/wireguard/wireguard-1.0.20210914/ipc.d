760dce102c781e41e34887dbed5425ef7d184e13
Mahtab Uddin <mahtabctg@gmail.com>
2022-08-21 08:17:15 +0000

    cleanup

---
+++ trunk/user/wireguard/wireguard-1.0.20210914/ipc.d
@@ -0,0 +1,20 @@
+ipc.o: ipc.c containers.h ipc.h ipc-uapi.h curve25519.h encoding.h \
+ ctype.h ipc-uapi-unix.h ipc-linux.h netlink.h
+
+containers.h:
+
+ipc.h:
+
+ipc-uapi.h:
+
+curve25519.h:
+
+encoding.h:
+
+ctype.h:
+
+ipc-uapi-unix.h:
+
+ipc-linux.h:
+
+netlink.h:
