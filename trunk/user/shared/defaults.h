6dd4c46ec53b848d14a93bef82e3b78f0f85adf3
Stepan Timofeev <stepatimofeev36@gmail.com>
2022-08-31 09:31:58 +0000

    Small fix for trunk/user/shared/defaults.h

--- trunk/user/shared/defaults.h
+++ trunk/user/shared/defaults.h
@@ -28,9 +28,9 @@
 #define SYS_USER_NOBODY		"nobody"
 #define SYS_GROUP_NOGROUP	"nogroup"
 
-#define DEF_LAN_ADDR		"192.168.0.1"
-#define DEF_LAN_DHCP_BEG	"192.168.0.2"
-#define DEF_LAN_DHCP_END	"192.168.0.244"
+#define DEF_LAN_ADDR		"192.168.1.1"
+#define DEF_LAN_DHCP_BEG	"192.168.1.2"
+#define DEF_LAN_DHCP_END	"192.168.1.244"
 #define DEF_LAN_MASK		"255.255.255.0"
 
 #define DEF_WLAN_2G_CC		"GB"
@@ -38,7 +38,7 @@
 #define DEF_WLAN_2G_SSID	"Padavan"
 #define DEF_WLAN_5G_SSID	"Padavan_5G"
 #define DEF_WLAN_2G_GSSID	"Padavan_GUEST"
-#define DEF_WLAN_5G_GSSID	"Padavan_5G_GUEST"
+#define DEF_WLAN_5G_GSSID	"Padavan_GUEST_5G"
 #define DEF_WLAN_2G_PSK		"1234567890"
 #define DEF_WLAN_5G_PSK		"1234567890"
 
