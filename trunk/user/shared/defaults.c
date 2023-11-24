b2666c7dc73ec49d73a0b63eb838b8dfbca23087
Sergey Hadzhioglu <evgeniyylysenko@gmail.com>
2023-11-24 07:58:05 +0000

    user: shared: added RethinkDNS & DNS resolvers use 1Hosts (Lite) for (DoH) Proxy

--- trunk/user/shared/defaults.c
+++ trunk/user/shared/defaults.c
@@ -609,15 +609,15 @@ struct nvram_pair router_defaults[] = {
 	{ "wins_enable", "0" },
 #if defined (APP_DOH)
 	{ "doh_enable", "0" },
-	{ "doh_server1", "https://blitz.ahadns.com/1:4" },
-	{ "doh_server2", "https://freedns.controld.com/x-1hosts-lite" },
-	{ "doh_server3", "" },
+	{ "doh_server1", "https://blitz.ahadns.com/1:4" },			/* DNS resolver use 1Hosts (Lite) */
+	{ "doh_server2", "https://freedns.controld.com/x-1hosts-lite" },	/* DNS resolver use 1Hosts (Lite) */
+	{ "doh_server3", "https://max.rethinkdns.com/1:AAQCAA==" },		/* DNS resolver use 1Hosts (Lite) */
 	{ "doh_opt1_1", "-4 -d -v" },
-	{ "doh_opt2_1", "1.1.1.1,8.8.8.8,9.9.9.9,208.67.222.222" },
+	{ "doh_opt2_1", "1.1.1.1,8.8.8.8,9.9.9.9,208.67.222.222" },		/* DNS servers to resolve resolver DoH Server 1 */
 	{ "doh_opt1_2", "-4 -d -v" },
-	{ "doh_opt2_2", "1.1.1.1,8.8.8.8,9.9.9.9,208.67.222.222" },
-	{ "doh_opt1_3", "" },
-	{ "doh_opt2_3", "" },
+	{ "doh_opt2_2", "1.1.1.1,8.8.8.8,9.9.9.9,208.67.222.222" },		/* DNS servers to resolve resolver DoH Server 2 */
+	{ "doh_opt1_3", "-4 -d -v" },
+	{ "doh_opt2_3", "1.1.1.1,8.8.8.8,9.9.9.9,208.67.222.222" },		/* DNS servers to resolve resolver DoH Server 3 */
 #endif
 #if defined (APP_STUBBY)
 	{ "stubby_enable", "0" },
