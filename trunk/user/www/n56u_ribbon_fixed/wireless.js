fc9bc4a76856fc1b8935436e05f14f85e75989c2
Sergey Hadzhioglu <evgeniyylysenko@gmail.com>
2023-12-07 17:52:39 +0000

    user: fix wireless.js

--- trunk/user/www/n56u_ribbon_fixed/wireless.js
+++ trunk/user/www/n56u_ribbon_fixed/wireless.js
@@ -479,10 +479,10 @@ function insertChannelOption() {
 
     if (country == "BY")
         channels = new Array(0, 36, 40, 44, 48, 52, 56, 60, 64, 132, 136, 140, 144); //Region 34
-        
+
     if (country == "UA")
         channels = new Array(0, 36, 40, 44, 48, 52, 56, 60, 64, 100, 104, 108, 112, 116, 120, 124, 128, 132, 136, 140, 149, 153, 157, 161, 165); //Region 35
-        
+
     if (country == "RU")
         channels = new Array(0, 36, 40, 44, 48, 52, 56, 60, 64, 132, 136, 140, 144, 149, 153, 157, 161, 165); //Region 36
 
