2632610e03374feba5ea234c6a1d90c1bdc709d6
Sergey Hadzhioglu <evgeniyylysenko@gmail.com>
2024-02-25 03:23:48 +0000

    Added Disable IPv6 DNS records

--- trunk/user/httpd/variables.c
+++ trunk/user/httpd/variables.c
@@ -495,6 +495,7 @@
 			{"dhcp_dnsv6_x", "", NULL, EVM_RESTART_DHCPD},
 			{"dhcp_wins_x", "", NULL, EVM_RESTART_DHCPD|EVM_REAPPLY_VPNSVR},
 			{"redirect_alldns", "", NULL, EVM_RESTART_FIREWALL|EVM_RESTART_DHCPD},
+			{"dhcp_filter_aaa", "", NULL, EVM_RESTART_DHCPD},
 			{"dhcp_verbose", "", NULL, EVM_RESTART_DHCPD},
 			{"dhcp_static_x", "", NULL, EVM_RESTART_DHCPD},
 			{"dhcp_static_arp", "", NULL, EVM_RESTART_DHCPD},
--- trunk/user/www/n56u_ribbon_fixed/help.js
+++ trunk/user/www/n56u_ribbon_fixed/help.js
@@ -24,6 +24,7 @@ helpcontent[5] = new Array("",
 				"<#LANHostConfig_x_LDNSServer6_itemdesc#>",
 				"<#LANHostConfig_x_WINSServer_itemdesc#>",
 				"<#LANHostConfig_ForceDNS_itemdesc#>",
+				"<#LANHostConfig_DHCPFilterAAAA_itemdesc#>",
 				"<#LANHostConfig_ManualDHCPEnable_itemdesc#>",
 				"<#LANHostConfig_ManualARP_itemdesc#>");
 helpcontent[6] = new Array("",
