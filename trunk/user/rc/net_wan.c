72db2b811ca367040c7b6a61b2f3aa67f0f2fde8
onkofonko <ocky852@gmail.com>
2024-07-13 13:40:58 +0200

    Minimize code for lan/wan fixes

--- trunk/user/rc/net_wan.c
+++ trunk/user/rc/net_wan.c
@@ -2619,8 +2619,7 @@ int start_udhcpc_wan(char *wan_ifname, int unit, int wait_lease)
 	if (strlen(wan_hostname) > 0) {
 		dhcp_argv[index++] = "-x";
 		char wan_mergedhostname[26] = "hostname:";
-		size_t remaining_space = sizeof(wan_mergedhostname) - strlen(wan_mergedhostname) - 1;
-		strncat(wan_mergedhostname, sanity_hostname(wan_hostname), remaining_space);
+		strncat(wan_mergedhostname, sanity_hostname(wan_hostname), sizeof(wan_mergedhostname) - strlen(wan_mergedhostname) - 1);
 		dhcp_argv[index++] = wan_mergedhostname;
 	}
 
