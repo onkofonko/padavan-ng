1e1d833c5c8a99fd23d8b907d20e06ddf8fc4377
Sergey Hadzhioglu <evgeniyylysenko@gmail.com>
2025-01-09 08:42:09 +0000

    dropbear: removed reference to dss_host_key which is no longer created by dropbear

--- trunk/user/dropbear/sshd.sh
+++ trunk/user/dropbear/sshd.sh
@@ -2,24 +2,20 @@
 
 dir_storage="/etc/storage/dropbear"
 rsa_key="$dir_storage/rsa_host_key"
-dss_key="$dir_storage/dss_host_key"
 ecdsa_key="$dir_storage/ecdsa_host_key"
 ed25519_key="$dir_storage/ed25519_host_key"
 
 func_createkeys()
 {
 	rm -f "$rsa_key"
-	rm -f "$dss_key"
 	rm -f "$ecdsa_key"
 	rm -f "$ed25519_key"
 
 	[ ! -d "$dir_storage" ] && mkdir -p -m 755 $dir_storage
 	/usr/bin/dropbearkey -t rsa -f "$rsa_key"
-	/usr/bin/dropbearkey -t dss -f "$dss_key"
 	/usr/bin/dropbearkey -t ecdsa -f "$ecdsa_key"
 	/usr/bin/dropbearkey -t ed25519 -f "$ed25519_key"
 	chmod 600 "$rsa_key"
-	chmod 600 "$dss_key"
 	chmod 600 "$ecdsa_key"
 	chmod 600 "$ed25519_key"
 }
@@ -32,11 +28,11 @@ func_start()
 	[ ! -d "$dir_storage" ] && mkdir -p -m 755 $dir_storage
 
 	old_pattern="/etc/storage/dropbear_"
-	for i in rsa_host_key dss_host_key ecdsa_host_key ; do
+	for i in rsa_host_key ed25519_host_key ecdsa_host_key ; do
 		[ -f "${old_pattern}_${i}" ] && mv -n "${old_pattern}_${i}" "$dir_storage/${i}"
 	done
 
-	if [ ! -f "$rsa_key" ] || [ ! -f "$dss_key" ]; then
+	if [ ! -f "$rsa_key" ] || [ ! -f "$ed25519_key" ]; then
 		func_createkeys
 	fi
 
