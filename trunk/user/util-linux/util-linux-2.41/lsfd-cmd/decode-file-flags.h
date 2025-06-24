3596f16897473d7dc0dd1e84763583aa24d9914a
Sergey Hadzhioglu <evgeniyylysenko@gmail.com>
2025-06-24 22:59:06 +0000

    util-linux: updated to 2.41.1

--- trunk/user/util-linux/util-linux-2.41/lsfd-cmd/decode-file-flags.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTIL_LINUX_LSFD_DECODE_FILE_FLAGS_H
-#define UTIL_LINUX_LSFD_DECODE_FILE_FLAGS_H
-
-#include "buffer.h"
-
-void lsfd_decode_file_flags(struct ul_buffer *buf, int flags);
-
-#endif /* UTIL_LINUX_LSFD_DECODE_FILE_FLAGS_H */
